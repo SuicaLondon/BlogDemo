71949dab723019b2be692d8b890be9eb58de5311
SuicaDavid <genglanghuang@gmail.com>
2021-06-03 19:35:43 +0100

    finish all function

--- models/articles.js
+++ models/articles.js
@@ -1,15 +1,19 @@
 const mongoose = require('mongoose')
 const marked = require('marked')
 const slugify = require('slugify')
+const createDomPurify = require('dompurify')
+const {JSDOM} = require('jsdom')
+const dompurify = createDomPurify(new JSDOM().window)
+
 const articlesSchema = new mongoose.Schema({
     title: {
         type: String,
         require: true
     },
-    title: {
+    description: {
         type: String
     },
-    title: {
+    markdown: {
         type: String,
         require: true
     },
@@ -21,12 +25,19 @@ const articlesSchema = new mongoose.Schema({
         type: String,
         require: true,
         unique: true
+    },
+    pureHTML: {
+        type: String,
+        require: true
     }
 })
 
 articlesSchema.pre('validate', function(next) {
     if(this.title) {
         this.slug = slugify(this.title, {lower: true, strict: true})
+    }
+    if(this.markdown) {
+        this.pureHTML = dompurify.sanitize(marked(this.markdown))
     }
     next()
 })
